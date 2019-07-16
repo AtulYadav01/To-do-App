780d1190f1185ebc52ef9f4af8b938313f95ae86
Atul Yadav <38830582+Ashu1533@users.noreply.github.com>
2019-07-16 17:24:25 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Todolist
+
+# Create your views here.
+
+def index(request):
+    todo_items = Todolist.objects.order_by('id')
+    context = {'todo_items' : todo_items}
+    return render(request,'todolist/index.html', context)
